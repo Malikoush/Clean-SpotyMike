a5cf180aa7a21f51cde9e1d317e2ddf0f1fc4ef3
Martin <ouedraogomartin12@yahoo.fr>
2024-05-28 16:38:43 +0200

    Edition logique fichier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    pathMatch: 'full',
+    redirectTo: 'login'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./shared/tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+
 ];
