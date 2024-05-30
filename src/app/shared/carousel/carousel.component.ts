305384c177b2f84452d4bc437c0071560c959ebf
Martin <ouedraogomartin12@yahoo.fr>
2024-05-30 16:16:41 +0200

    composant_carousel

---
+++ src/app/shared/carousel/carousel.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Song } from '../../core/interfaces/song';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CarouselComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  @Input() songs!:Song[];
+
+}
